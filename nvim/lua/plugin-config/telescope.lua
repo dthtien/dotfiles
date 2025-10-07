cd99412875df8ad64dccde6d77e74e72e2272c8b
Dau The Tien <tiendt2311@gmail.com>
2025-10-07 22:23:34 +1100

    nvim: update telescope text

--- nvim/lua/plugin-config/telescope.lua
+++ nvim/lua/plugin-config/telescope.lua
@@ -1,7 +1,18 @@
 require('telescope').setup({
   defaults = {
     file_ignore_patterns = { "node_modules", ".git/" },
-    layout_strategy = 'bottom_pane'
+    layout_strategy = 'bottom_pane',
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+      "--glob=!.git/",
+    },
   },
   pickers = {
     find_files = {
